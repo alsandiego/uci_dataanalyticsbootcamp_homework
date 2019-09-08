a1311b5b97e09c9c2ee1431c20d2e55fb2b89665
Al San Diego <hun@Als-MacBook-Pro.local>
2019-09-07 21:03:02 -0700

    used jupyter notebook for PyPoll

---
+++ Python/PyPoll/main.py
@@ -0,0 +1,16 @@
+import os
+import csv
+
+#path of csv
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # # Read each row of data after the header
+    # for row in csvreader:
+    #     print(row)
