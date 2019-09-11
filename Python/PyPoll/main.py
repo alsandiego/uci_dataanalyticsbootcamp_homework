95fdbb56515b8d31f9c702f0f59b2b468597a8cf
Al San Diego <hun@Als-MacBook-Pro.local>
2019-09-10 21:22:27 -0700

    Completed PyPoll

--- Python/PyPoll/main.py
+++ Python/PyPoll/main.py
@@ -1,16 +1,54 @@
-import os
+#import dependencies needed
 import csv
+import os
 
-#path of csv
+#reference my csv file
 csvpath = os.path.join('Resources', 'election_data.csv')
+#specify file to output
+outputpath = os.path.join("output", "summary.csv")
+
+candidates = {}
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first (skip this step if there is now header)
+    #Read the header row first and then skip it
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    
+    for row in csvreader:
+        #check if candidate exists and then count votes
+        if row[2] not in candidates:
+            candidates[row[2]] = 1
+        else:
+            candidates[row[2]] += 1
+    
+    #get total votes
+    totalvotes = sum(candidates.values())
+    #get Key(candidate) with highest value
+    winner = max(candidates, key=candidates.get)
+    
+    print('Election Results')
+    print('-------------------------')
+    print('Total Votes: ' + str(totalvotes))
+    print('-------------------------')
+    for k,v in candidates.items():
+        print(str(k) + ': ' + str("{:.3%}".format(v/totalvotes)) +'% ('+ str(v) +')')
+    print('-------------------------')
+    print('Winner: '+str(winner))
+    print('-------------------------')
 
-    # # Read each row of data after the header
-    # for row in csvreader:
-    #     print(row)
+# Open the output file
+with open(outputpath, 'w', newline='') as csvfile:
+
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    
+    #write each row
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(['-------------------------'])
+    csvwriter.writerow(['Total Votes: ' + str(totalvotes)])
+    csvwriter.writerow(['-------------------------'])
+    for k,v in candidates.items():
+       csvwriter.writerow([(str(k) + ': ' + str("{:.3%}".format(v/totalvotes)) +'% ('+ str(v) +')')])
+    csvwriter.writerow(['-------------------------'])
+    csvwriter.writerow(['Winner: '+str(winner)])
+    csvwriter.writerow(['-------------------------'])
